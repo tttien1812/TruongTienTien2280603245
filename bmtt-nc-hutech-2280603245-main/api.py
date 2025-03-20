1524d8340c2f01e6dc62ab4adfab3ea0825d015a
tttien1812 <tttien204@gmail.com>
2025-03-20 16:18:03 +0700

    LAB03_caesar

--- bmtt-nc-hutech-2280603245-main/api.py
+++ bmtt-nc-hutech-2280603245-main/api.py
@@ -1,30 +1,30 @@
-# from flask import Flask, request, jsonify
-# from cipher.caesar import CaesarCipher
+from flask import Flask, request, jsonify
+from cipher.caesar import CaesarCipher
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# # CAESAR CIPHER ALGORITHM
-# caesar_cipher = CaesarCipher()
+# CAESAR CIPHER ALGORITHM
+caesar_cipher = CaesarCipher()
 
-# @app.route("/api/caesar/encrypt", methods=["POST"])
-# def caesar_encrypt():
-#     data = request.json
-#     plain_text = data['plain_text']
-#     key = int(data['key'])
-#     encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
-#     return jsonify({'encrypted_message': encrypted_text})
+@app.route("/api/caesar/encrypt", methods=["POST"])
+def caesar_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
 
-# @app.route("/api/caesar/decrypt", methods=["POST"])
-# def caesar_decrypt():
-#     data = request.json
-#     cipher_text = data['cipher_text']
-#     key = int(data['key'])
-#     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
-#     return jsonify({'decrypted_message': decrypted_text})
+@app.route("/api/caesar/decrypt", methods=["POST"])
+def caesar_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
 
-# # main function
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+# main function
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 # ---------------------------------VIGENERE-----------------------------------------
 # from flask import Flask, request, jsonify
@@ -118,27 +118,27 @@
 #     app.run(host="0.0.0.0", port=5000, debug=True)
 
 # ------------------------------------TRANSPOSITION---------------------------------------
-from cipher.transposition import TranspositionCipher
-from flask import Flask, request, jsonify
+# from cipher.transposition import TranspositionCipher
+# from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-transposition_cipher = TranspositionCipher()
+# transposition_cipher = TranspositionCipher()
 
-@app.route('/api/transposition/encrypt', methods=['POST'])
-def transposition_encrypt():
-    data = request.get_json()
-    plain_text = data.get('plain_text')
-    key = int(data.get('key'))
-    encrypted_text = transposition_cipher.encrypt(plain_text, key)
-    return jsonify({'encrypted_text': encrypted_text})
+# @app.route('/api/transposition/encrypt', methods=['POST'])
+# def transposition_encrypt():
+#     data = request.get_json()
+#     plain_text = data.get('plain_text')
+#     key = int(data.get('key'))
+#     encrypted_text = transposition_cipher.encrypt(plain_text, key)
+#     return jsonify({'encrypted_text': encrypted_text})
 
-@app.route('/api/transposition/decrypt', methods=['POST'])
-def transposition_decrypt():
-    data = request.get_json()
-    cipher_text = data.get('cipher_text')
-    key = int(data.get('key'))
-    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
-    return jsonify({'decrypted_text': decrypted_text})
-if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
+# @app.route('/api/transposition/decrypt', methods=['POST'])
+# def transposition_decrypt():
+#     data = request.get_json()
+#     cipher_text = data.get('cipher_text')
+#     key = int(data.get('key'))
+#     decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+#     return jsonify({'decrypted_text': decrypted_text})
+# if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=5000, debug=True)
